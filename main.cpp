7bcb1626f53b53d39db86aa0fae3fb605a9b207a
Juan Mercurio <juanmercuriobadii@gmail.com>
2023-09-13 09:57:34 -0300

    First Neuron and prediction with no training 	- we have a cost, feed foward function

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdio.h>
 
 typedef struct Neuron {
@@ -5,11 +6,33 @@ typedef struct Neuron {
 	float bias;
 } Neuron;
 
+float sigmoid(float x) {
+	return 1.0 / (1 + std::exp(-x));
+}
+
+float feed_foward(Neuron n, float input) {
+	float total = n.weight * input + n.bias;
+	return sigmoid(total);
+}
+
+float cost_function(float value, float prediction) {
+	float error = value - prediction;
+	float cost = error * error / 1;		// the 1 is the amount of inputs
+	return cost;
+}
 
 int main (int argc, char *argv[]) {
+
 	float input = 3.0;
-	Neuron n = {3.2, 2};
-	
-	printf("Termino el programa");
+	Neuron n = {3.0, 2.0};
+
+	float value = 3.0;
+	float prediction = feed_foward(n, input);
+	float cost = cost_function(value, prediction);
+
+	printf("The prediction is: %f\n", prediction);
+	printf("the value is %f\n", value);
+	printf("The cost is: %f\n", cost);
+
 	return 0;
 }
