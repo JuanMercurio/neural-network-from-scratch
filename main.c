ddb7f1dc78254b68bf865249493d08f215c5e122
Juan Mercurio <juanmercuriobadii@gmail.com>
2023-09-25 23:04:13 -0300

    matrix dot product working (tested only a few cases)

--- main.c
+++ main.c
@@ -13,8 +13,9 @@ typedef struct {
 } Matrix;
 
 typedef struct {
-  Matrix **weights;
+  Matrix **list_weights; // its a list of matrices
   int matrix_weight_count;
+  Matrix **neurons;
 } NeuralNetwork;
 
 void matrix_times_scalar(Matrix *matrix, float scalar) {
@@ -59,19 +60,50 @@ void weights_initilize(Matrix *matrix) {
       // TODO: BEST random between
     }
   }
-
-  // normalize values with the sqrt of the amount of weights
-  matrix_times_scalar(matrix, sqrt((double)matrix->rows * matrix->cols));
 }
 
 float sigmoid(float x) { return 1.0 / (1 - exp(-x)); }
 float sigmoid_derivative(float x) { return x * (1 - x); }
 
-void fit(Matrix *inputs, Matrix *outputs, int epochs, int display_update) {
+Matrix *matrix_dot(Matrix *m1, Matrix *m2) {
 
-  insert_ones_column(inputs);
+  Matrix *final = matrix_allocate(m1->rows, m2->cols);
+
+  for (int i = 0; i < final->rows; i++) {
+    for (int j = 0; j < final->cols; j++) {
+      float value = 0;
+      for (int k = 0; k < final->cols; k++) {
+        value += m1->data[i][k] * m2->data[k][j];
+      }
+      final->data[i][j] = value;
+    }
+  }
+  return final;
 }
 
+// void fit_partial(NeuralNetwork nn, Matrix *input, float *output) {
+//
+//   int neurons_count = nn.list_weights[0]->cols; // this is static for now
+//   float *activations = malloc(sizeof(float) * neurons_count);
+//   *activations = *input;
+//
+//   for (int i = 0; i < nn.matrix_weight_count; i++) {
+//   }
+// }
+//
+// void fit(NeuralNetwork nn, Matrix *inputs, Matrix *outputs, int epochs,
+//          int display_update) {
+//   insert_ones_column(inputs);
+//
+//   for (int i = 0; i < epochs; i++) {
+//     fit_partial(nn, inputs, outputs);
+//   }
+//
+//   if (epochs == 0 || epochs + 1 % display_update == 0) {
+//     printf("saturnoooooooooooooo\n");
+//   }
+// }
+//
 NeuralNetwork create_neural_network(int layers[], int len_layers,
                                     float learning_rate) {
 
@@ -88,6 +120,8 @@ NeuralNetwork create_neural_network(int layers[], int len_layers,
     Matrix *m = matrix_allocate(row_count, col_count);
 
     weights_initilize(m);
+    // normalize values with the sqrt of the amount of weights
+    matrix_times_scalar(m, sqrt((double)m->rows * m->cols));
     weights[i] = m;
 
     count_matrix++;
@@ -100,6 +134,9 @@ NeuralNetwork create_neural_network(int layers[], int len_layers,
   Matrix *last_weights = matrix_allocate(row_count, col_count);
 
   weights_initilize(last_weights);
+  // normalize values with the sqrt of the amount of weights
+  matrix_times_scalar(last_weights,
+                      sqrt((double)last_weights->rows * last_weights->cols));
   weights[len_layers - 2] = last_weights;
   count_matrix++;
 
@@ -108,16 +145,26 @@ NeuralNetwork create_neural_network(int layers[], int len_layers,
   return nn;
 }
 
+Matrix *matrix_create_random(int rows, int cols) {
+  Matrix *m = matrix_allocate(rows, cols);
+  weights_initilize(m);
+  return m;
+}
+
+void matrix_print(Matrix *m) {
+  for (int j = 0; j < m->rows; j++) {
+    for (int k = 0; k < m->cols; k++) {
+      printf("%-10g ", m->data[j][k]);
+    }
+    printf("\n");
+  }
+}
+
 void print_weights(NeuralNetwork nn) {
 
   printf("\n");
   for (int i = 0; i < nn.matrix_weight_count; i++) {
-    for (int j = 0; j < nn.weights[i]->rows; j++) {
-      for (int k = 0; k < nn.weights[i]->cols; k++) {
-        printf("%-10g ", nn.weights[i]->data[j][k]);
-      }
-      printf("\n");
-    }
+    matrix_print(nn.list_weights[i]);
     printf("\n");
   }
 }
@@ -126,11 +173,30 @@ int main(int argc, char *argv[]) {
 
   float learning_rate = 0.1;
   int layers[] = {2, 2, 1};
+
+  Matrix *inputs = matrix_create_random(2, 2);
+  Matrix *outputs = matrix_create_random(2, 2);
+
   int len_layers = sizeof(layers) / sizeof(layers[0]);
 
-  NeuralNetwork nn = create_neural_network(layers, len_layers, learning_rate);
-  // printf("%d\n", nn.matrix_weight_count);
-  print_weights(nn);
+  // NeuralNetwork nn = create_neural_network(layers, len_layers,
+  // learning_rate);
+
+  Matrix *a = matrix_allocate(2, 2);
+  a->data[0][0] = 0.0;
+  a->data[0][1] = 1.0;
+  a->data[1][0] = 1.0;
+  a->data[1][1] = 0.0;
+
+  Matrix *b = matrix_allocate(1, 2);
+  b->data[0][0] = 1.0;
+  b->data[0][1] = 1.0;
+
+  Matrix *result = matrix_dot(b, a);
+  matrix_print(result);
+
+  // print_weights(nn);
+  // fit(nn, inputs, outputs, 1, 100);
 
   return 0;
 }
